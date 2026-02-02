5fdc9bde8417dfdfe310ff94d3115a3eacf23cf6
wesleypspontes-coder <ezequias.diamond@gmail.com>
2026-02-02 10:44:27 -0300

    first commit

---
+++ src/test/java/br/com/cotiinformatica/api_clientes/ApiClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica.api_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
